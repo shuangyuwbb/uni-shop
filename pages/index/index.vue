82343bd3cf0319d566aa9e8ec23fb6edbcb9262a
wubinbin <wubinbin@fscut.com>
2020-11-17 18:57:55 +0800

    first

---
+++ pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="container">
+		<uni-search-bar placeholder="自定placeholder" @confirm="search"></uni-search-bar>
+		<view class="intro"></view>
+		<text class="intro"></text>
+		<uni-link :href="href" :text="href"></uni-link>
+	</view>
+</template>
+
+<script>
+	import uniSearchBar from '@/components/uni-search-bar/uni-search-bar.vue'
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		components:{
+			uniSearchBar
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
